e7e9147f9d433f0a95fc05e5e4e7f521a26fe420
zp_field <zp_field@163.com>
2018-01-09 11:53:34 +0800

    生成文件

---
+++ nuxt.config.js
@@ -0,0 +1,66 @@
+module.exports = {
+  dev: (process.env.NODE_ENV !== 'production'),
+  css: [
+    '~assets/main.css',
+  ],
+  /**
+   ** Headers of the page
+   */
+  head: {
+    title: 'nuxt-text-01',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', type: 'text/css', href: 'https://cdn.bootcss.com/element-ui/2.0.10/theme-chalk/index.css' }
+    ]
+  },
+  /**
+   ** Customize the progress bar color
+   */
+  loading: {
+    color: '#3B8070',
+    failedColor: '#cf000e',
+    height: '5px',
+    duration: 5000
+  },
+  /**
+   ** Build configuration
+   */
+  build: {
+    /**
+     ** Run ESLint on save
+     */
+    extend (config, ctx) {
+      if (ctx.dev && ctx.isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
+      }
+    },
+    loaders: [
+      {
+        test: /\.(png|jpe?g|gif|svg)$/,
+        loader: 'url-loader',
+        query: {
+          limit: 10000,
+          name: 'img/[name].[hash].[ext]'
+        }
+      },
+    ],
+  },
+  plugins: [
+    { src: '~plugins/vue-ElementUI', ssr: false }
+  ],
+  /**
+   * 组件缓存
+   * 默认状态下缓存1000个组件，缓存时间15分钟
+   * */
+  cache: true,
+}
---
+++ pages/user/index.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="User">
+    <el-form v-model="form" label-position="left" label-width="100px">
+      <el-form-item prop="name" label="用户名：">
+        <el-input autoComplete="off" name="name" type="text" v-model="form.name"></el-input>
+      </el-form-item>
+      <el-form-item prop="pwd" label="密码:">
+        <el-input autoComplete="off" name="pwd" type="password" v-model="form.pwd"></el-input>
+      </el-form-item>
+      <el-button type="primary" @click="submitForm" :loading="submitState">提&nbsp&nbsp&nbsp&nbsp交</el-button>
+    </el-form>
+  </div>
+</template>
+
+<script>
+  'use strict'
+  export default {
+    layout () {
+      return 'default'
+    },
+    name: 'User',
+    data () {
+      return {
+        submitState: false,
+        form: {
+          name: '',
+          pwd: ''
+        }
+      }
+    },
+    methods: {
+      submitForm () {
+        this.submitState = true
+        setTimeout(() => {
+          this.submitState = false
+        }, 5000)
+      }
+    }
+  }
+</script>
+
+<style scoped lang="less">
+  .User {
+    width: 80%;
+    margin: 30px auto;
+    .el-button {
+      width: 60%;
+      display: block;
+      margin: auto;
+    }
+  }
+</style>
